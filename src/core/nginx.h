8137f94c4a7518aad78f0f7ac2cd31b90eb0026a
jesset <jesse.tian@me.com>
2020-07-30 10:57:04 +0800

    ver

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -11,7 +11,7 @@
 
 #define nginx_version      1019001
 #define NGINX_VERSION      "1.19.1"
-#define NGINX_VER          "nginx/" NGINX_VERSION
+#define NGINX_VER          "nginx"
 
 #ifdef NGX_BUILD
 #define NGINX_VER_BUILD    NGINX_VER " (" NGX_BUILD ")"
